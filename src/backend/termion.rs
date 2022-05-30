9a33cea146036847307cb3b037b086a65aa968c9
Arijit Basu <sayanarijit@gmail.com>
2022-05-30 18:12:47 +0530

    Add tests for tab

--- src/backend/termion.rs
+++ src/backend/termion.rs
@@ -63,3 +63,17 @@ pub fn write<W: Write>(
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn handle_tab() {
+        let evt = Event::Key(Key::Char('\t'));
+
+        let req = to_input_request(&evt);
+
+        assert!(req.is_none());
+    }
+}
