710017a4c29bbffffe1b04548c2bf2756f427023
Arijit Basu <sayanarijit@gmail.com>
2023-05-31 20:01:26 +0530

    Update ratatui example

--- examples/ratatui-input/src/main.rs
+++ examples/ratatui-input/src/main.rs
@@ -11,7 +11,7 @@ use tui::{
     backend::{Backend, CrosstermBackend},
     layout::{Constraint, Direction, Layout},
     style::{Color, Modifier, Style},
-    text::{Span, Spans, Text},
+    text::{Line, Span, Text},
     widgets::{Block, Borders, List, ListItem, Paragraph},
     Frame, Terminal,
 };
@@ -139,7 +139,7 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
             Style::default(),
         ),
     };
-    let mut text = Text::from(Spans::from(msg));
+    let mut text = Text::from(Line::from(msg));
     text.patch_style(style);
     let help_message = Paragraph::new(text);
     f.render_widget(help_message, chunks[0]);
@@ -178,7 +178,7 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
         .iter()
         .enumerate()
         .map(|(i, m)| {
-            let content = vec![Spans::from(Span::raw(format!("{}: {}", i, m)))];
+            let content = vec![Line::from(Span::raw(format!("{}: {}", i, m)))];
             ListItem::new(content)
         })
         .collect();
