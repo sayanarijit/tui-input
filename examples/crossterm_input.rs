26e2bf58c9fac6eda8092174568bf05ae3aec9b9
Josh McKinney <joshka@users.noreply.github.com>
2025-03-02 13:49:33 -0800

    feat: rewrite crossterm example
    Fix up crossterm code by:
    - ensure errors when rendering / reading do not prevent restoring terminal
    - handle accept / cancel of the prompt
    - add a label (change logic to prompt for a name, and greet the person)
    - import crossterm instead of ratatui::crossterm to make it clear that this is not a ratatui example
    - remove mouse input
    - decompose into logical functions
    - add comments

--- examples/crossterm_input.rs
+++ examples/crossterm_input.rs
@@ -1,52 +1,75 @@
-use ratatui::crossterm::{
-    cursor::{Hide, Show},
-    event::{read, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEvent},
-    execute,
+//! This example demonstrates how to use the `tui_input` crate with the `crossterm` backend.
+//! The example prompts the user for their name and prints a greeting.
+//! The user can cancel the input by pressing `Esc` or accept the input by pressing `Enter`.
+
+use std::io::{self, stdout, Write};
+
+use crossterm::{
+    cursor::{Hide, MoveTo, Show},
+    event::{read, Event, KeyCode, KeyEvent},
+    execute, queue,
+    style::Print,
     terminal::{
         disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
     },
 };
-use std::io::{stdout, Result, Write};
-use tui_input::backend::crossterm as backend;
-use tui_input::backend::crossterm::EventHandler;
-use tui_input::Input;
+use tui_input::{
+    backend::crossterm::{self as backend, EventHandler},
+    Input,
+};
 
-fn main() -> Result<()> {
+fn main() -> io::Result<()> {
     enable_raw_mode()?;
-    let stdout = stdout();
-    let mut stdout = stdout.lock();
-    execute!(stdout, Hide, EnterAlternateScreen, EnableMouseCapture)?;
+    let mut stdout = stdout().lock();
+    execute!(stdout, Hide, EnterAlternateScreen)?;
 
-    let mut input: Input = "Hello ".into();
-    backend::write(&mut stdout, input.value(), input.cursor(), (0, 0), 15)?;
-    stdout.flush()?;
+    let name = get_user_name(&mut stdout);
+
+    execute!(stdout, Show, LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+
+    match name? {
+        Some(name) => println!("Hello {name}!"),
+        None => println!("Goodbye!"),
+    }
+    Ok(())
+}
+
+/// Prompts the user for their name.
+///
+/// Returns `None` if the user cancels the input otherwise returns the user's name. If the user
+/// presses `Esc` the input is cancelled. If the user presses `Enter` the input is accepted.
+///
+/// # Errors
+///
+/// Returns an error if reading or writing to the terminal fails.
+fn get_user_name(stdout: &mut impl Write) -> io::Result<Option<String>> {
+    let mut input = Input::from("World");
+    render_prompt(stdout, &input)?;
 
     loop {
         let event = read()?;
-
         if let Event::Key(KeyEvent { code, .. }) = event {
             match code {
-                KeyCode::Esc | KeyCode::Enter => {
-                    break;
-                }
+                KeyCode::Esc => return Ok(None),
+                KeyCode::Enter => return Ok(Some(input.to_string())),
                 _ => {
                     if input.handle_event(&event).is_some() {
-                        backend::write(
-                            &mut stdout,
-                            input.value(),
-                            input.cursor(),
-                            (0, 0),
-                            15,
-                        )?;
-                        stdout.flush()?;
+                        render_prompt(stdout, &input)?;
                     }
                 }
             }
         }
     }
+}
 
-    execute!(stdout, Show, LeaveAlternateScreen, DisableMouseCapture)?;
-    disable_raw_mode()?;
-    println!("{}", input);
+fn render_prompt(stdout: &mut impl Write, input: &Input) -> io::Result<()> {
+    const LABEL: &str = "Name: ";
+    const POSITION: (u16, u16) = (LABEL.len() as u16, 0);
+    const WIDTH: u16 = 15;
+
+    queue!(stdout, MoveTo(0, 0), Print(LABEL))?;
+    backend::write(stdout, input.value(), input.cursor(), POSITION, WIDTH)?;
+    stdout.flush()?;
     Ok(())
 }
