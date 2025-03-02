fd09fdb5b3a8f3e69a3224030965729802136fe4
Josh McKinney <joshka@users.noreply.github.com>
2025-03-02 14:07:30 -0800

    feat: rewrite termion example
    Fix up termion code by:
    - ensuring that errors when rendering / reading do not prevent restoring terminal
    - handle accept / cancel of the prompt
    - add a label (change logic to prompt for a name, and greet the person)
    - decompose into logical functions
    - add comments

--- examples/termion_input.rs
+++ examples/termion_input.rs
@@ -1,38 +1,67 @@
-use std::io::{stdin, stdout, Result, Write};
-use termion::cursor::{Hide, Show};
-use termion::event::{Event, Key};
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
-use termion::screen::IntoAlternateScreen;
-use tui_input::backend::termion as backend;
-use tui_input::backend::termion::EventHandler;
-use tui_input::Input;
+//! This example demonstrates how to use the `tui_input` crate with the `crossterm` backend.
+//! The example prompts the user for their name and prints a greeting.
+//! The user can cancel the input by pressing `Esc` or accept the input by pressing `Enter`.
 
-fn main() -> Result<()> {
-    let mut input: Input = "Hello ".into();
-    {
-        let stdin = stdin();
-        let mut stdout = stdout().into_raw_mode()?.into_alternate_screen()?;
+use std::io::{self, stdin, stdout, Write};
 
-        write!(&mut stdout, "{}", Hide)?;
-        backend::write(&mut stdout, input.value(), input.cursor(), (0, 0), 15)?;
-        stdout.flush()?;
+use termion::{
+    cursor::{Goto, Hide, Show},
+    event::{Event, Key},
+    input::TermRead,
+    raw::IntoRawMode,
+    screen::IntoAlternateScreen,
+};
+use tui_input::{
+    backend::termion::{self as backend, EventHandler},
+    Input,
+};
 
-        for evt in stdin.events() {
-            let evt = evt?;
-            if evt == Event::Key(Key::Esc) || evt == Event::Key(Key::Char('\n')) {
-                break;
-            }
+fn main() -> io::Result<()> {
+    let mut stdout = stdout().into_raw_mode()?.into_alternate_screen()?;
+    write!(stdout, "{}", Hide)?;
+    let name = get_user_name(&mut stdout);
+    write!(stdout, "{}", Show)?;
+    drop(stdout); // disable raw mode and leave alternate screen
 
-            if input.handle_event(&evt).is_some() {
-                backend::write(&mut stdout, input.value(), input.cursor(), (0, 0), 15)?;
-                stdout.flush()?;
+    match name? {
+        Some(name) => println!("Hello {name}!"),
+        None => println!("Goodbye!"),
+    }
+    Ok(())
+}
+
+/// Prompts the user for their name.
+///
+/// Returns `None` if the user cancels the input otherwise returns the user's name. If the user
+/// presses `Esc` the input is cancelled. If the user presses `Enter` the input is accepted.
+///
+/// # Errors
+///
+/// Returns an error if reading or writing to the terminal fails.
+fn get_user_name(stdout: &mut impl Write) -> io::Result<Option<String>> {
+    let mut input: Input = "World".into();
+    render_prompt(stdout, &input)?;
+
+    for event in stdin().events() {
+        match event? {
+            Event::Key(Key::Esc) => return Err(io::Error::other("error")),
+            Event::Key(Key::Char('\n')) => return Ok(Some(input.to_string())),
+            event => {
+                if input.handle_event(&event).is_some() {
+                    render_prompt(stdout, &input)?;
+                }
             }
         }
-
-        write!(stdout, "{}", Show)?;
     }
+    Ok(None) // reached end of input
+}
 
-    println!("{}", input);
+fn render_prompt(stdout: &mut impl Write, input: &Input) -> io::Result<()> {
+    const LABEL: &str = "Name: ";
+    const POSITION: (u16, u16) = (LABEL.len() as u16, 0);
+
+    write!(stdout, "{}{LABEL}", Goto(1, 1))?;
+    backend::write(stdout, input.value(), input.cursor(), POSITION, 15)?;
+    stdout.flush()?;
     Ok(())
 }
